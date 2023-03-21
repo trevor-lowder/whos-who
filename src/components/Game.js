451b6643df768e6ea9327e8e48666047703ba722
TSL-coding <lowder.trevor@yahoo.com>
2023-03-21 14:42:54 -0500

    update object shape

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,53 +2,51 @@ import React, { useState } from "react";
 
 const Game = ({
   genre = "pop",
-  numSongs = 1,
   numArtists = 2,
+  numSongs = 1,
+  songs,
+  artists,
   numAttempts = 3,
 }) => {
-  const artists = [
+  artists = [
     {
-      id: 1,
-      name: "Artist 1",
-      image: "https://via.placeholder.com/150",
+      artistName: "Artist 1",
+      artistImg: "https://via.placeholder.com/150",
     },
     {
-      id: 2,
-      name: "Artist 2",
-      image: "https://via.placeholder.com/150",
+      artistName: "Artist 2",
+      artistImg: "https://via.placeholder.com/150",
     },
     {
-      id: 3,
-      name: "Artist 3",
-      image: "https://via.placeholder.com/150",
+      artistName: "Artist 3",
+      artistImg: "https://via.placeholder.com/150",
     },
     {
-      id: 4,
-      name: "Artist 4",
-      image: "https://via.placeholder.com/150",
+      artistName: "Artist 4",
+      artistImg: "https://via.placeholder.com/150",
     },
   ];
 
-  const songs = [
+  songs = [
     {
-      id: 1,
-      title: "Song 1",
-      artistId: 1,
+      songName: "Song 1",
+      artistName: "Artist 1",
+      previewURL: 1,
     },
     {
-      id: 2,
-      title: "Song 2",
-      artistId: 2,
+      songName: "Song 2",
+      artistName: "Artist 2",
+      previewURL: 2,
     },
     {
-      id: 3,
-      title: "Song 3",
-      artistId: 3,
+      songName: "Song 3",
+      artistName: "Artist 3",
+      previewURL: 3,
     },
     {
-      id: 4,
-      title: "Song 4",
-      artistId: 4,
+      songName: "Song 4",
+      artistName: "Artist 4",
+      previewURL: 4,
     },
   ];
 
@@ -66,21 +64,21 @@ const Game = ({
   const handleSelectSong = (song) => {
     setSelectedSong(song);
     if (selectedArtist !== null) {
-      if (song.artistId !== selectedArtist.id) {
+      if (song.artistName !== selectedArtist.name) {
         setAttempts(attempts - 1);
+        setSelectedSong(null);
+        setSelectedArtist(null);
       }
-      setSelectedSong(null);
-      setSelectedArtist(null);
     }
   };
   const handleSelectArtist = (artist) => {
     setSelectedArtist(artist);
     if (selectedSong !== null) {
-      if (selectedSong.artistId !== artist.id) {
+      if (selectedSong.artistName !== artist.name) {
         setAttempts(attempts - 1);
+        setSelectedSong(null);
+        setSelectedArtist(null);
       }
-      setSelectedSong(null);
-      setSelectedArtist(null);
     }
   };
 
@@ -92,9 +90,9 @@ const Game = ({
         <h2>Songs</h2>
         <div>
           {selectedSongs.map((song) => (
-            <div key={song.id}>
+            <div key={song.artistName}>
               <button onClick={() => handleSelectSong(song)}>
-                <p>{song.title}</p>
+                <p>{song.songName}</p>
               </button>
             </div>
           ))}
@@ -102,12 +100,12 @@ const Game = ({
         <h2>Artists</h2>
         <div>
           {selectedArtists.map((artist) => (
-            <div key={artist.id}>
+            <div key={artist.artistName}>
               <div>
-                <img src={artist.image} alt={artist.name} />
+                <img src={artist.artistImg} alt={artist.artistName} />
               </div>
               <button onClick={() => handleSelectArtist(artist)}>
-                <p>{artist.name}</p>
+                <p>{artist.artistName}</p>
               </button>
             </div>
           ))}
